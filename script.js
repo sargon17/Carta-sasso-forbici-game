00c310317cd79463f74395887010b390297a40c5
sargon17 <mykhaylo.tymofyeyev@gmail.com>
2022-01-06 11:04:31 +0100

    working on the project

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+/*jshint esversion: 6 */
+
 let cartaBtn = document.querySelector("#Carta__btn");
 let sassoBtn = document.querySelector("#Sasso__btn");
 let forbiciBtn = document.querySelector("#Forbici__btn");
 let playerStatusSign = document.querySelector("#player__status");
 let computerChoice = document.querySelector("#computer__choice");
 let humanChoice = document.querySelector("#human__choice");
+let computerCounter = document.querySelector("#computer__counter");
+let playerCounter = document.querySelector("#player__counter");
+let gameStoryList = document.querySelector("#game__story__lists");
+let betterOfThreeButton = document.querySelector("#btrOfThree");
+let resultDisplaySection = document.querySelector(".win_loss_result");
+let restartButton = document.querySelector("#restart__button")
+let endGameButton = document.querySelector("#end__game");
 
+ computerPoints = 0;
+ humanPoints = 0;
+ gameCounter = 0;
 
-let computerPoints = 0;
-let humanPoints = 0;
-
+let betterOfThree = false;
 
 function cartaSassoForbici(selection) {
     let compSelection = ["carta","sasso","forbici"];
@@ -40,9 +50,18 @@ function cartaSassoForbici(selection) {
             playerStatus = "vinto";
         }
     }
-    console.log(playerStatus)
+    gameCounter++;
+    console.log(playerStatus);
     resultDisplay(playerStatus,randSelection,selection);
-    
+    counterUpdate(playerStatus);
+    updateStoryList(playerStatus, randSelection, selection, computerPoints, humanPoints);
+
+    if(betterOfThree){
+        if(gameCounter === 3){
+            winnerChecker(humanPoints,computerPoints);
+        }
+    }
+
 }
 
 
@@ -50,18 +69,89 @@ function resultDisplay(playerStat,computer,human){
     playerStatusSign.innerHTML = "Hai " + playerStat;
     computerChoice.innerHTML = "computer: " + computer;
     humanChoice.innerHTML = "human: " + human;
-}
+};
 
 function counterUpdate(playerStat){
     if(playerStat === "vinto"){
-        humanPoints++
+        humanPoints++;
+        playerCounter.innerHTML = humanPoints;
     } else if (playerStat === "perso"){
         computerPoints++
-    }
-    console.log(computerPoints);
-    console.log(humanPoints)
+        computerCounter.innerHTML = computerPoints;
+    };
+;}
+
+function counterReset(){
+    gameCounter = 0;
+    humanPoints = 0;
+    computerPoints = 0;
+    playerCounter.innerHTML = humanPoints;
+    computerCounter.innerHTML = computerPoints;
 }
 
+
+function updateStoryList(playerStat,computerChoice,humanChoice,computerPoints,humanPoints){
+    let newRow = document.createElement("li");
+    let newDiv = document.createElement("div");
+    let result = document.createElement("p");
+    let points = document.createElement("p");
+    let resultString = document.createElement("p");
+
+    newRow.setAttribute("class","game__story");
+    newDiv.setAttribute("class","game__results");
+    newDiv.classList.add("center");
+    result.innerHTML = `Hai ${playerStat}`;
+    result.classList.add("bold");
+    points.innerHTML = `${computerPoints} : ${humanPoints}`;
+    resultString.innerHTML = `Computer: <strong>${computerChoice}</strong> human: <strong>${humanChoice}</strong>`;
+
+
+    if(playerStat === "vinto"){
+        result.classList.add("green__victory");
+    } else if(playerStat === "perso"){
+        result.classList.add("red__lost");
+    } else{
+        result.classList.add("grey__draw")
+    };
+
+    newRow.append(newDiv);
+    newDiv.append(result);
+    newDiv.append(points);
+    newRow.append(resultString)
+
+    gameStoryList.prepend(newRow);
+};
+
+function winnerChecker(playerPoints,computerPoints){
+    if(playerPoints > computerPoints){
+        showResult("vinto",computerPoints,playerPoints);
+    } else if(playerPoints < computerPoints){
+        showResult("perso",computerPoints,playerPoints);
+    } else {
+        showResult("pareggiato",computerPoints,playerPoints);
+    };
+
+};
+
+
+function showResult(playerStat,computerPoints,playerPoints){
+    document.querySelector("#text_result").innerHTML = `Hai ${playerStat}`;
+    document.querySelector("#number_result").innerHTML = `${computerPoints} : ${playerPoints}`;
+    resultDisplaySection.classList.remove("undisplay");
+}
+
+
+function checkBtn(){
+    if(betterOfThree){
+        betterOfThreeButton.classList.add("in__game");
+    } else{
+        betterOfThreeButton.classList.remove("in__game");
+    }
+};
+
+function endGame(playerPoints,computerPoints){
+    winnerChecker(playerPoints,computerPoints);
+}
 
 
 
@@ -70,3 +160,19 @@ function counterUpdate(playerStat){
 cartaBtn.addEventListener("click", ()=> {cartaSassoForbici("carta")});
 sassoBtn.addEventListener("click", ()=> {cartaSassoForbici("sasso")});
 forbiciBtn.addEventListener("click", ()=> {cartaSassoForbici("forbici")});
+betterOfThreeButton.addEventListener("click", () => {
+    if(betterOfThree === true){
+        betterOfThree = false;
+    } else {
+        betterOfThree = true;
+    }
+    checkBtn();
+    counterReset();
+});
+restartButton.addEventListener("click", () => {
+    resultDisplaySection.classList.add("undisplay");
+    counterReset();
+});
+endGameButton.addEventListener("click", () => {
+    endGame(humanPoints,computerPoints);
+});
